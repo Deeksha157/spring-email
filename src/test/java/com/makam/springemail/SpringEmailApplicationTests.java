2393bfdfd78ddbc895d0fb3564905a05cb24391c
Deeksha <deekshamakam47@gmail.com>
2023-06-14 16:50:21 +0200

    Initial commit

---
+++ src/test/java/com/makam/springemail/SpringEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makam.springemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
